7b89c6bff537976cde03b9b271be3705993cc254
LarsDD0815 <132018238+LarsDD0815@users.noreply.github.com>
2023-09-23 10:52:38 +0200

    Auto fährt in alle Richtungen

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
+music.play(music.stringPlayable("C5 A G F G A B C5 ", 120), music.PlaybackMode.UntilDone)
+basic.showLeds(`
+    . # . # .
+    # # # # #
+    # # # # #
+    . # # # .
+    . . # . .
+    `)
+irRemote.connectInfrared(DigitalPin.P0)
 basic.forever(function () {
-	
+    if (irRemote.returnIrButton() == 70) {
+        mecanumRobotV2.MotorenVorwärts(20)
+    } else if (irRemote.returnIrButton() == 68) {
+        mecanumRobotV2.LinksDrehen(18)
+    } else if (irRemote.returnIrButton() == 67) {
+        mecanumRobotV2.RechtsDrehen(18)
+    } else if (irRemote.returnIrButton() == 21) {
+        mecanumRobotV2.MotorenRückwärts(20)
+    } else {
+        mecanumRobotV2.MotorenAnhalten()
+    }
 })
