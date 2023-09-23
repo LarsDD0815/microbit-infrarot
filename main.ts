688536e2ec2b61274a6baa5d56bc4d670bd6ab58
LarsDD0815 <132018238+LarsDD0815@users.noreply.github.com>
2023-09-23 19:02:23 +0200

    maximalgeschwindigkeit per variable gesetzt + soft-brake

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-music.play(music.stringPlayable("C5 A G F G A B C5 ", 120), music.PlaybackMode.UntilDone)
+let MaximaleGeschwindigkeit = 70
 basic.showLeds(`
     . # . # .
     # # # # #
@@ -6,16 +6,39 @@ basic.showLeds(`
     . # # # .
     . . # . .
     `)
+music.play(music.stringPlayable("C5 A G F G A B C5 ", 120), music.PlaybackMode.UntilDone)
 irRemote.connectInfrared(DigitalPin.P0)
 basic.forever(function () {
+    if (input.isGesture(Gesture.Shake)) {
+        basic.showLeds(`
+            . # # # .
+            # . # . #
+            # # # # #
+            . # # # .
+            . # # # .
+            `)
+        for (let index = 0; index < 4; index++) {
+            music.play(music.createSoundExpression(WaveShape.Noise, 54, 54, 255, 160, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+        }
+    } else {
+        basic.showLeds(`
+            . # . # .
+            # # # # #
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+    }
+})
+basic.forever(function () {
     if (irRemote.returnIrButton() == 70) {
-        mecanumRobotV2.MotorenVorwärts(20)
-    } else if (irRemote.returnIrButton() == 68) {
-        mecanumRobotV2.LinksDrehen(18)
+        mecanumRobotV2.MotorenVorwärts(MaximaleGeschwindigkeit)
+    } else if ((0 as any) == (68 as any)) {
+        mecanumRobotV2.LinksDrehen(MaximaleGeschwindigkeit)
     } else if (irRemote.returnIrButton() == 67) {
-        mecanumRobotV2.RechtsDrehen(18)
+        mecanumRobotV2.RechtsDrehen(MaximaleGeschwindigkeit)
     } else if (irRemote.returnIrButton() == 21) {
-        mecanumRobotV2.MotorenRückwärts(20)
+        mecanumRobotV2.MotorenRückwärts(MaximaleGeschwindigkeit)
     } else {
         mecanumRobotV2.MotorenAnhalten()
     }
