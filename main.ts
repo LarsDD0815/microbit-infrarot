1de00f173e28b47ef1615b971add0e3e97854d7c
LarsDD0815 <132018238+LarsDD0815@users.noreply.github.com>
2023-12-29 17:38:46 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,11 @@
-music.play(music.stringPlayable("C5 A G F G A B C5 ", 120), music.PlaybackMode.UntilDone)
+mecanumRobotV2.setServo(0)
+let LichtAn = 22
+let Vorwärts = 70
+let Rückwärs = 21
+let RechtsDrehen = 67
+let LinksDrehen = 68
+let LichtAus = 25
+let MaximaleGeschwindigkeit = 70
 basic.showLeds(`
     . # . # .
     # # # # #
@@ -6,16 +13,23 @@ basic.showLeds(`
     . # # # .
     . . # . .
     `)
+music.play(music.stringPlayable("C5 A G F G A B C5 ", 120), music.PlaybackMode.UntilDone)
 irRemote.connectInfrared(DigitalPin.P0)
 basic.forever(function () {
-    if (irRemote.returnIrButton() == 70) {
-        mecanumRobotV2.MotorenVorwärts(20)
-    } else if (irRemote.returnIrButton() == 68) {
-        mecanumRobotV2.LinksDrehen(18)
-    } else if (irRemote.returnIrButton() == 67) {
-        mecanumRobotV2.RechtsDrehen(18)
-    } else if (irRemote.returnIrButton() == 21) {
-        mecanumRobotV2.MotorenRückwärts(20)
+    if (irRemote.returnIrButton() == Vorwärts) {
+        mecanumRobotV2.MotorenVorwärts(MaximaleGeschwindigkeit)
+    } else if (irRemote.returnIrButton() == LinksDrehen) {
+        mecanumRobotV2.LinksDrehen(MaximaleGeschwindigkeit)
+    } else if (irRemote.returnIrButton() == RechtsDrehen) {
+        mecanumRobotV2.RechtsDrehen(MaximaleGeschwindigkeit)
+    } else if (irRemote.returnIrButton() == Rückwärs) {
+        mecanumRobotV2.MotorenRückwärts(MaximaleGeschwindigkeit)
+    } else if (irRemote.returnIrButton() == LichtAn) {
+        mecanumRobotV2.setLed(LED.Left, LEDColor.Rainbow)
+        mecanumRobotV2.setLed(LED.Right, LEDColor.Rainbow)
+    } else if (irRemote.returnIrButton() == LichtAus) {
+        mecanumRobotV2.setLed(LED.Right, LEDColor.Off)
+        mecanumRobotV2.setLed(LED.Left, LEDColor.Off)
     } else {
         mecanumRobotV2.MotorenAnhalten()
     }
