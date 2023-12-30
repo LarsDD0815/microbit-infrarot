c5404d1d8c8b19ae49c857499a325846e69a206a
LarsDD0815 <132018238+LarsDD0815@users.noreply.github.com>
2023-12-30 15:58:06 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,12 @@
+let MaximaleGeschwindigkeit = 0
 mecanumRobotV2.stelleServo(0)
 let LichtAn = 22
 let Vorwärts = 70
 let Rückwärs = 21
 let RechtsDrehen = 67
 let LinksDrehen = 68
-let LichtAus = 25
-let MaximaleGeschwindigkeit = 70
+let LichtAus = MaximaleGeschwindigkeit
+MaximaleGeschwindigkeit = 70
 basic.showLeds(`
     . # . # .
     # # # # #
@@ -16,12 +17,17 @@ basic.showLeds(`
 music.play(music.stringPlayable("C5 A G F G A B C5 ", 120), music.PlaybackMode.UntilDone)
 irRemote.connectInfrared(DigitalPin.P0)
 basic.forever(function () {
+    let Drehgeschwindigkeit = 0
     if (irRemote.returnIrButton() == Vorwärts) {
-        mecanumRobotV2.motorenVorwärts(MaximaleGeschwindigkeit)
+        if (mecanumRobotV2.aktuelleEntfernungInZentimetern() > 30) {
+            mecanumRobotV2.motorenVorwärts(MaximaleGeschwindigkeit)
+        } else {
+            mecanumRobotV2.motorenAnhalten()
+        }
     } else if (irRemote.returnIrButton() == LinksDrehen) {
-        mecanumRobotV2.linksDrehen(MaximaleGeschwindigkeit)
+        mecanumRobotV2.linksDrehen(Drehgeschwindigkeit)
     } else if (irRemote.returnIrButton() == RechtsDrehen) {
-        mecanumRobotV2.rechtsDrehen(MaximaleGeschwindigkeit)
+        mecanumRobotV2.rechtsDrehen(Drehgeschwindigkeit)
     } else if (irRemote.returnIrButton() == Rückwärs) {
         mecanumRobotV2.motorenRückwärts(MaximaleGeschwindigkeit)
     } else if (irRemote.returnIrButton() == LichtAn) {
