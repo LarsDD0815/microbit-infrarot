080002b1ad1894a01c42acd6c4a198e7c9af8bac
LarsDD0815 <132018238+LarsDD0815@users.noreply.github.com>
2023-12-30 16:10:22 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
-let MaximaleGeschwindigkeit = 0
 mecanumRobotV2.stelleServo(0)
-let LichtAn = 22
 let Vorwärts = 70
 let Rückwärs = 21
 let RechtsDrehen = 67
 let LinksDrehen = 68
-let LichtAus = MaximaleGeschwindigkeit
-MaximaleGeschwindigkeit = 70
+let LichtAn = 22
+let LichtAus = 25
+let MaximaleGeschwindigkeit = 70
+let Drehgeschwindigkeit = 30
 basic.showLeds(`
     . # . # .
     # # # # #
@@ -14,10 +14,8 @@ basic.showLeds(`
     . # # # .
     . . # . .
     `)
-music.play(music.stringPlayable("C5 A G F G A B C5 ", 120), music.PlaybackMode.UntilDone)
 irRemote.connectInfrared(DigitalPin.P0)
 basic.forever(function () {
-    let Drehgeschwindigkeit = 0
     if (irRemote.returnIrButton() == Vorwärts) {
         if (mecanumRobotV2.aktuelleEntfernungInZentimetern() > 30) {
             mecanumRobotV2.motorenVorwärts(MaximaleGeschwindigkeit)
